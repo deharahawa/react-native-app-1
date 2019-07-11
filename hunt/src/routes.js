408a0de238dc00e82179a41bb94ac666567428fb
deh410 <andre.arahawa@cromai.com>
2019-07-11 12:50:23 -0300

    main page and routes

---
+++ hunt/src/routes.js
@@ -0,0 +1,9 @@
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+import Main from './pages/main';
+
+export default createAppContainer(
+  createStackNavigator({
+    Main
+  })
+);
