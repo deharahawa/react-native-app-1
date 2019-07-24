1c92db224ad79f266af3597bffd8edd6f66f0857
deh410 <andre.arahawa@cromai.com>
2019-07-24 14:24:36 -0300

    add styles

--- hunt/src/routes.js
+++ hunt/src/routes.js
@@ -5,5 +5,12 @@ import Main from './pages/main';
 export default createAppContainer(
   createStackNavigator({
     Main
-  })
+  }, {
+      navigationOptions: {
+        headerStyle: {
+          backgroundColor: "#DA552F"
+        },
+        headerTintColor: "#FFF"
+      }
+    })
 );
